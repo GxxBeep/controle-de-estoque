0e1dd9b91007ccfaddad63b99dce8f81287920bf
Gil Santos <52254467+GxxBeep@users.noreply.github.com>
2024-07-28 21:40:35 -0300

    Implmentando a função cadastro, melhorias em upload de imagens

--- route.py
+++ route.py
@@ -1,28 +1,120 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
+from dotenv import load_dotenv
+import os
+
+
+UPLOAD_FOLDER = 'static/img'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('KEY')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/cadastrar-produto')
-def cadastrar_produto():
-    title = "Cadastrar Produto"
-    className = 'ClassName = "op-cadastro-produto"'
-    description = "Em manutenção..."
 
-    template = render_template(
-        'menu-nav/manutencao.html',
-        title=title,
-        className=className,
-        description=description
-        )
+
+
+
+
+
+
+
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/cadastrar-produto/', methods=('GET', 'POST'))
+def cadastrar_produto():
+
+    categoria = ['Blusas', 'Calças', 'Camisas',
+                'Casacos', 'Jaquetas', 'Macacões', 
+                'Saias', 'Sapatos', 'Shorts', 'Vestidos']
+    
+    #TODO: Espeficar tamanho para cada categoria
+    camisa_tam_fem = ''
+    camisa_tam_mas = ''
+
+    if request.method == 'POST':
+        value = request.form['produtoNome']
+        if not value:
+            flash('Não foi')
+        else:
+            print(request.form['produtoNome'])
+            print(request.form['categoria'])
+
+            if 'image' not in request.files:
+                flash('Sem arquivos')
+                return redirect(request.url)
+            
+            file = request.files['image']
+            
+            if file.filename == '':
+                flash('Nenhum arquivo selecionado')
+                return redirect(request.url)
+            
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+                return redirect(request.url)
+    
+                
+
+    template = render_template('menu-nav/cadastro.html', categoria=categoria)
 
     return template
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# Pendente a configuraçáo
+#
 @app.route('/editar-produto')
 def editar_produto():
     title = "Editar informações do Produto"
@@ -39,6 +131,9 @@ def editar_produto():
     return template
 
 
+#
+# Pendente a configuraçáo
+#
 @app.route('/remover-produto')
 def remover_produto():
     title = "Remover um Produto"
@@ -55,6 +150,9 @@ def remover_produto():
     return template
 
 
+#
+# Pendente a configuraçáo
+#
 @app.route('/listar-produtos')
 def listar_produtos():
     title = "Listar todos os Produtos"
@@ -71,6 +169,9 @@ def listar_produtos():
     return template
 
 
+#
+# Pendente a configuraçáo
+#
 @app.route('/info-produto')
 def info_produto():
     title = "Informações do Produto"
@@ -88,4 +189,5 @@ def info_produto():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    load_dotenv()
+    app.run(debug=True, host='26.45.19.217')
