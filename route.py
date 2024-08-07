ddaf726e159583c64f12558da32015c12f5f287c
fabianomonteirofarias <famofa61131561@gmail.com>
2024-08-06 23:57:18 -0300

    Organização e implementação da routa para o Menu de Informações.

--- route.py
+++ route.py
@@ -17,24 +17,26 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # Pendente a configuraçáo
 #
 @app.route('/')
-def index():
+def index() -> str:
     return render_template('index.html')
 
 
-def allowed_file(filename):
+def allowed_file(filename) -> bool:
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 #
 # Pendente a configuraçáo
 #
 @app.route('/cadastrar-produto/', methods=('GET', 'POST'))
-def cadastrar_produto():
+def cadastrar_produto() -> str:
 
-    categoria = ['Blusas', 'Calças', 'Camisas',
-                'Casacos', 'Jaquetas', 'Macacões', 
-                'Saias', 'Sapatos', 'Shorts', 'Vestidos']
-    
-    #TODO: Espeficar tamanho para cada categoria
+    categoria: list[str] = ['Blusas', 'Calças', 'Camisas',
+                            'Casacos', 'Jaquetas', 'Macacões',
+                            'Saias', 'Sapatos', 'Shorts', 'Vestidos']
+
+    # TODO: Espeficar tamanho para cada categoria
     camisa_tam_fem = ''
     camisa_tam_mas = ''
 
@@ -49,19 +51,18 @@ def cadastrar_produto():
             if 'image' not in request.files:
                 flash('Sem arquivos')
                 return redirect(request.url)
-            
+
             file = request.files['image']
-            
+
             if file.filename == '':
                 flash('Nenhum arquivo selecionado')
                 return redirect(request.url)
-            
+
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+                file.save(os.path.join(
+                    app.config['UPLOAD_FOLDER'], file.filename))
                 return redirect(request.url)
-    
-                
 
     template = render_template('menu-nav/cadastro.html', categoria=categoria)
 
@@ -72,69 +73,62 @@ def cadastrar_produto():
 # Pendente a configuraçáo
 #
 @app.route('/editar-produto')
-def editar_produto():
+def editar_produto() -> str:
 
     template = render_template('menu-nav/editor.html')
 
     return template
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #
 # Pendente a configuraçáo
 #
 @app.route('/listar-produtos')
-def listar_produtos():
-
+def listar_produtos() -> str:
     template = render_template('menu-nav/list-product.html')
-
     return template
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 #
 # Pendente a configuraçáo
 #
 @app.route('/info-produto')
-def info_produto():
+def info_produto() -> str:
+    # TODO: Obter dicionário com uma função GET
+    dictionary: dict[str, str] = {
+        # produto 1
+        'titulo': 'Caneta Azul',
+        'categoria': 'Azul Caneta',
+        'publico': 'Feminino',
+        'saldo': '10',
+        'tamanho': 'GG',
+        'descricao': 'Um texto qualquer.',
+        # produto 2
+        'codigo': '1234567890',
+        'fornecedor': 'Google Ltda',
+        'precofornecedor': '1000.00',
+        'precovenda': '10.00'
+    }
+    imagens: list[str] = ["/static/img/image.jpg",
+                    "/static/img/image.jpg",
+                    "/static/img/image.jpg",
+                    "/static/img/image.jpg",
+                    "/static/img/image.jpg"]
+    template: str = render_template(
+        'menu-nav/info-product.html',
+        dictionary=dictionary,
+        imagens=imagens)
+
+    return template
+
+
+@app.route('/login')
+def login() -> str:
+    return render_template('login.html')
+
+
+@app.route('/manutencao')
+def manutencao() -> str:
     title = "Informações do Produto"
     className = 'ClassName = "op-info-produto"'
     description = "Em manutenção..."
@@ -144,27 +138,9 @@ def info_produto():
         title=title,
         className=className,
         description=description
-        )
+    )
 
     return template
-
-
-
-
-
-
-
-
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
-
-
-
 
 
 if __name__ == '__main__':
