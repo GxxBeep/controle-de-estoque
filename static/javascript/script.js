6841a2e9563b3ed2632640d8dc41b6aa0058b853
Liu <52254467+GxxBeep@users.noreply.github.com>
2025-02-18 18:14:58 -0300

    Implementando função para manipular aba 'menu-nav'

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,9 +1,8 @@
-/* TODO: CADASTRO 3 - EXIBIR IMAGENS AO ADICIONAR,
-    E ADICIONAR DESCRIÇÃO DA IMAGEM */
-
-
- /* TODO: Criar função para redimencionar imagens no grid
-    Cadastro de Protudo, ElementoID: cadastroProduto-3
-    Exemplo: 
-        width = (Altura / 10)
-        heigth = (Largura / 10) */
+function open_menu() {
+    let menu = document.querySelector(".menu-nav")
+    if (menu.getAttribute("enable")) {
+        menu.removeAttribute("enable")
+    } else {
+        menu.setAttribute("enable", "true")
+    }
+}
